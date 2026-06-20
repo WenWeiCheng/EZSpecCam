48cffe60209812335ed5b4f9ab65d8ee12bed393
weichen <2645464082@qq.com>
2026-06-20 19:21:42 +0800

    feat(scale-dialog): wire spectrum X/Y range mode controls

--- src/gui/widgets/dialogs/ScaleControlDialog.cpp
+++ src/gui/widgets/dialogs/ScaleControlDialog.cpp
@@ -2,8 +2,10 @@
 
 #include <QVBoxLayout>
 #include <QFormLayout>
+#include <QHBoxLayout>
 #include <QGroupBox>
 #include <QLabel>
+#include <QDoubleSpinBox>
 
 ScaleControlDialog::ScaleControlDialog(QWidget *parent)
     : QDialog(parent)
@@ -60,8 +62,78 @@ void ScaleControlDialog::createSpectrumGroup()
     m_spectrumScaleTypeCombo->addItem("log", 1);
     layout->addRow("Type:", m_spectrumScaleTypeCombo);
 
+    m_spectrumXRangeModeCombo = new QComboBox(this);
+    m_spectrumXRangeModeCombo->addItem("Auto", 0);
+    m_spectrumXRangeModeCombo->addItem("Manual", 1);
+    layout->addRow("X Range:", m_spectrumXRangeModeCombo);
+
+    m_spectrumManualXMinSpin = new QDoubleSpinBox(this);
+    m_spectrumManualXMinSpin->setDecimals(2);
+    m_spectrumManualXMinSpin->setRange(-1000000.0, 1000000.0);
+    m_spectrumManualXMinSpin->setValue(0.0);
+
+    m_spectrumManualXMaxSpin = new QDoubleSpinBox(this);
+    m_spectrumManualXMaxSpin->setDecimals(2);
+    m_spectrumManualXMaxSpin->setRange(-1000000.0, 1000000.0);
+    m_spectrumManualXMaxSpin->setValue(100.0);
+
+    auto *xManualLayout = new QHBoxLayout();
+    xManualLayout->addWidget(m_spectrumManualXMinSpin);
+    xManualLayout->addWidget(m_spectrumManualXMaxSpin);
+    layout->addRow("X Min / Max:", xManualLayout);
+
+    m_spectrumYRangeModeCombo = new QComboBox(this);
+    m_spectrumYRangeModeCombo->addItem("Auto", 0);
+    m_spectrumYRangeModeCombo->addItem("Manual", 1);
+    layout->addRow("Y Range:", m_spectrumYRangeModeCombo);
+
+    m_spectrumManualYMinSpin = new QDoubleSpinBox(this);
+    m_spectrumManualYMinSpin->setDecimals(2);
+    m_spectrumManualYMinSpin->setRange(-1000000.0, 1000000.0);
+    m_spectrumManualYMinSpin->setValue(0.0);
+
+    m_spectrumManualYMaxSpin = new QDoubleSpinBox(this);
+    m_spectrumManualYMaxSpin->setDecimals(2);
+    m_spectrumManualYMaxSpin->setRange(-1000000.0, 1000000.0);
+    m_spectrumManualYMaxSpin->setValue(100.0);
+
+    auto *yManualLayout = new QHBoxLayout();
+    yManualLayout->addWidget(m_spectrumManualYMinSpin);
+    yManualLayout->addWidget(m_spectrumManualYMaxSpin);
+    layout->addRow("Y Min / Max:", yManualLayout);
+
     connect(m_spectrumScaleTypeCombo, QOverload<int>::of(&QComboBox::currentIndexChanged),
             this, &ScaleControlDialog::onSpectrumScaleTypeChanged);
+    connect(m_spectrumXRangeModeCombo, QOverload<int>::of(&QComboBox::currentIndexChanged),
+            this, &ScaleControlDialog::onSpectrumXRangeModeChanged);
+    connect(m_spectrumYRangeModeCombo, QOverload<int>::of(&QComboBox::currentIndexChanged),
+            this, &ScaleControlDialog::onSpectrumYRangeModeChanged);
+
+    connect(m_spectrumManualXMinSpin, QOverload<double>::of(&QDoubleSpinBox::valueChanged),
+            this, [this](double) { onSpectrumManualXMinChanged(); });
+    connect(m_spectrumManualXMaxSpin, QOverload<double>::of(&QDoubleSpinBox::valueChanged),
+            this, [this](double) { onSpectrumManualXMaxChanged(); });
+    connect(m_spectrumManualYMinSpin, QOverload<double>::of(&QDoubleSpinBox::valueChanged),
+            this, [this](double) { onSpectrumManualYMinChanged(); });
+    connect(m_spectrumManualYMaxSpin, QOverload<double>::of(&QDoubleSpinBox::valueChanged),
+            this, [this](double) { onSpectrumManualYMaxChanged(); });
+
+    updateSpectrumManualXEnabled();
+    updateSpectrumManualYEnabled();
+}
+
+void ScaleControlDialog::updateSpectrumManualXEnabled()
+{
+    bool manual = m_spectrumXRangeModeCombo->currentData().toInt() == 1;
+    m_spectrumManualXMinSpin->setEnabled(manual);
+    m_spectrumManualXMaxSpin->setEnabled(manual);
+}
+
+void ScaleControlDialog::updateSpectrumManualYEnabled()
+{
+    bool manual = m_spectrumYRangeModeCombo->currentData().toInt() == 1;
+    m_spectrumManualYMinSpin->setEnabled(manual);
+    m_spectrumManualYMaxSpin->setEnabled(manual);
 }
 
 void ScaleControlDialog::setImageScaleType(int type)
@@ -94,6 +166,48 @@ void ScaleControlDialog::setSpectrumScaleType(int type)
     }
 }
 
+void ScaleControlDialog::setSpectrumXRangeMode(int mode)
+{
+    int index = m_spectrumXRangeModeCombo->findData(mode);
+    if (index >= 0) {
+        m_spectrumXRangeModeCombo->blockSignals(true);
+        m_spectrumXRangeModeCombo->setCurrentIndex(index);
+        m_spectrumXRangeModeCombo->blockSignals(false);
+        updateSpectrumManualXEnabled();
+    }
+}
+
+void ScaleControlDialog::setSpectrumYRangeMode(int mode)
+{
+    int index = m_spectrumYRangeModeCombo->findData(mode);
+    if (index >= 0) {
+        m_spectrumYRangeModeCombo->blockSignals(true);
+        m_spectrumYRangeModeCombo->setCurrentIndex(index);
+        m_spectrumYRangeModeCombo->blockSignals(false);
+        updateSpectrumManualYEnabled();
+    }
+}
+
+void ScaleControlDialog::setSpectrumManualXRange(double min, double max)
+{
+    m_spectrumManualXMinSpin->blockSignals(true);
+    m_spectrumManualXMaxSpin->blockSignals(true);
+    m_spectrumManualXMinSpin->setValue(min);
+    m_spectrumManualXMaxSpin->setValue(max);
+    m_spectrumManualXMinSpin->blockSignals(false);
+    m_spectrumManualXMaxSpin->blockSignals(false);
+}
+
+void ScaleControlDialog::setSpectrumManualYRange(double min, double max)
+{
+    m_spectrumManualYMinSpin->blockSignals(true);
+    m_spectrumManualYMaxSpin->blockSignals(true);
+    m_spectrumManualYMinSpin->setValue(min);
+    m_spectrumManualYMaxSpin->setValue(max);
+    m_spectrumManualYMinSpin->blockSignals(false);
+    m_spectrumManualYMaxSpin->blockSignals(false);
+}
+
 int ScaleControlDialog::imageScaleType() const
 {
     return m_imageScaleTypeCombo->currentData().toInt();
@@ -107,6 +221,36 @@ int ScaleControlDialog::imageColorScaleMode() const
 int ScaleControlDialog::spectrumScaleType() const
 {
     return m_spectrumScaleTypeCombo->currentData().toInt();
+}
+
+int ScaleControlDialog::spectrumXRangeMode() const
+{
+    return m_spectrumXRangeModeCombo->currentData().toInt();
+}
+
+int ScaleControlDialog::spectrumYRangeMode() const
+{
+    return m_spectrumYRangeModeCombo->currentData().toInt();
+}
+
+double ScaleControlDialog::spectrumManualXMin() const
+{
+    return m_spectrumManualXMinSpin->value();
+}
+
+double ScaleControlDialog::spectrumManualXMax() const
+{
+    return m_spectrumManualXMaxSpin->value();
+}
+
+double ScaleControlDialog::spectrumManualYMin() const
+{
+    return m_spectrumManualYMinSpin->value();
+}
+
+double ScaleControlDialog::spectrumManualYMax() const
+{
+    return m_spectrumManualYMaxSpin->value();
 }
 
 void ScaleControlDialog::onImageScaleTypeChanged(int index)
@@ -125,4 +269,52 @@ void ScaleControlDialog::onSpectrumScaleTypeChanged(int index)
 {
     Q_UNUSED(index)
     emit spectrumScaleTypeChanged(spectrumScaleType());
+}
+
+void ScaleControlDialog::onSpectrumXRangeModeChanged(int index)
+{
+    Q_UNUSED(index)
+    updateSpectrumManualXEnabled();
+    emit spectrumXRangeModeChanged(spectrumXRangeMode());
+    if (spectrumXRangeMode() == 1) {
+        emit spectrumManualXRangeChanged(spectrumManualXMin(), spectrumManualXMax());
+    }
+}
+
+void ScaleControlDialog::onSpectrumYRangeModeChanged(int index)
+{
+    Q_UNUSED(index)
+    updateSpectrumManualYEnabled();
+    emit spectrumYRangeModeChanged(spectrumYRangeMode());
+    if (spectrumYRangeMode() == 1) {
+        emit spectrumManualYRangeChanged(spectrumManualYMin(), spectrumManualYMax());
+    }
+}
+
+void ScaleControlDialog::onSpectrumManualXMinChanged()
+{
+    if (spectrumXRangeMode() == 1) {
+        emit spectrumManualXRangeChanged(spectrumManualXMin(), spectrumManualXMax());
+    }
+}
+
+void ScaleControlDialog::onSpectrumManualXMaxChanged()
+{
+    if (spectrumXRangeMode() == 1) {
+        emit spectrumManualXRangeChanged(spectrumManualXMin(), spectrumManualXMax());
+    }
+}
+
+void ScaleControlDialog::onSpectrumManualYMinChanged()
+{
+    if (spectrumYRangeMode() == 1) {
+        emit spectrumManualYRangeChanged(spectrumManualYMin(), spectrumManualYMax());
+    }
+}
+
+void ScaleControlDialog::onSpectrumManualYMaxChanged()
+{
+    if (spectrumYRangeMode() == 1) {
+        emit spectrumManualYRangeChanged(spectrumManualYMin(), spectrumManualYMax());
+    }
 }
